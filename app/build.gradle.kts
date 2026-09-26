e81d5add80dc06bbe92bf5ea4132d41eedaa9df5
Stellher <45536210+Stellher@users.noreply.github.com>
2026-09-26 22:13:27 +0800

    release: bump APK version to 0.6.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.logvarremote"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.6.0"
+        versionCode = 8
+        versionName = "0.6.1"
 
         ndk {
             abiFilters += listOf("arm64-v8a", "armeabi-v7a", "x86_64")
